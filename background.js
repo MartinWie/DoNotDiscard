51873c85fb750da90483bb01ef89e8bc89afafa0
MartinWie <martin.wiechmann.office@gmail.com>
2021-03-24 10:13:49 +0100

    removed review and support from original version

--- background.js
+++ background.js
@@ -14,26 +14,3 @@ chrome.runtime.onInstalled.addListener(function(details) {
   });
 });
 
-
-chrome.runtime.setUninstallURL("https://chrome.google.com/webstore/detail/dnhngfnfolbmhgealdpolmhimnoliiok/support");
-
-chrome.contextMenus.create({
-  id: "review",
-  title: "🌟 " + chrome.i18n.getMessage("cm_review"),
-  contexts: ["browser_action"]
-});
-chrome.contextMenus.create({
-  id: "support",
-  title: "💬 " + chrome.i18n.getMessage("cm_support"),
-  contexts: ["browser_action"]
-});
-chrome.contextMenus.onClicked.addListener(function(info, tab) {
-  switch(info.menuItemId) {
-    case "review":
-    chrome.tabs.create({url: "https://chrome.google.com/webstore/detail/dnhngfnfolbmhgealdpolmhimnoliiok/reviews"});
-    break;
-    case "support":
-    chrome.tabs.create({url: "https://chrome.google.com/webstore/detail/dnhngfnfolbmhgealdpolmhimnoliiok/support"});
-    break;
-  }
-});
