27d3fdb9fa9ab7a31cc96d5957d1829041c40fc1
MartinWie <martin.wiechmann.office@gmail.com>
2021-03-25 14:52:41 +0100

    added another Listener

--- background.js
+++ background.js
@@ -11,6 +11,12 @@ chrome.tabs.onReplaced.addListener(function(tabId) {
   }
 });
 
+chrome.tabs.onActivated.addListener(function(tab) {
+  if (tab.url.indexOf('troy') != -1){
+    chrome.tabs.update(tab.id, {autoDiscardable: false});
+  }
+});
+
 chrome.runtime.onInstalled.addListener(function(details) {
   chrome.tabs.query({}, function(tabs) {
     tabs.forEach(function(tab) {
